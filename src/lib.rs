85a3c7efa7e23a3132f04b467ef8e84545442530
David L. Inman <davidlevere@gmail.com>
2020-10-26 22:50:22 -0400

    initial stuff for lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+
+extern crate parse_input;
+extern crate error_reporter;
+
+use parse_input::{Input, ParseError, PSym};
+use error_reporter::ErrorReport;
+
+#[derive(Debug)]
+pub enum Data {
+    Nil, 
+    Cons { name : String
+         , params : Vec<Data>
+         },
+
+}
+
+pub fn parse_data( input : &str ) -> Result<Data, ErrorReport> {
+    Ok(Data::Nil)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
